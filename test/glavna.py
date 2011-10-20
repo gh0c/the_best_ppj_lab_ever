6958fd323ea2a8028031f348918da90a8c7db286
Goran <Gogo@(none)>
2011-10-20 12:31:36 +0200

    maknut print

---
+++ test/glavna.py
@@ -0,0 +1,11 @@
+import pretvorba
+from pretvorba import *
+def main():
+    print ('Bok')
+    x = input('Unesi regex: \n')
+    
+    print (x)
+
+    pretvori (x)
+
+if __name__ == "__main__": main()
